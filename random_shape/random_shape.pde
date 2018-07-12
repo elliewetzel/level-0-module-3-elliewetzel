80821e827a0c2208ba1e95ecde2054d3e1eee34d
elliewetzel <league@WorthyMini-5.attlocal.net>
2018-07-12 11:52:50 -0700

    Level 0, Module 3.  Obedient robot, bullseye, and bumble bee.

---
+++ random_shape/random_shape.pde
@@ -0,0 +1,7 @@
+void setup() {
+    size(500, 500);
+}
+void draw() {
+ ellipse(250, 400, 200, 200);
+ 
+}
--- src/modulo/ObedientRobot.java
+++ src/modulo/ObedientRobot.java
@@ -1,27 +1,64 @@
 package modulo;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-	public static void main(String[] args) {
-		Robot kate = new Robot();
+	static Robot kate;
+	public static void main(String[] args) {	
+		kate = new Robot();
 		kate.setSpeed(700);
 		kate.penDown();
-		for(int i = 0; i < 5; i++) {
-			kate.move(100);
-			kate.turn(90);
+		String color = JOptionPane.showInputDialog("What color do you want the pen to be?");
+		if(color.equals("red")) {
+			kate.setPenColor(Color.red);
 		}
-		kate.turn(90);
-	}
-
-	private static void drawTriangle() {
-		// TODO Auto-generated method stub
+		else if(color.equals("blue")) {
+			kate.setPenColor(Color.blue);
+		}
+		else if(color.equals("green")) {
+			kate.setPenColor(Color.green);
+		}
+		else if(color.equals("black")) {
+			kate.setPenColor(Color.black);
+		}
+		else if(color.equals("cyan")) {
+			kate.setPenColor(Color.cyan);
+		}
+		else {
+			kate.setRandomPenColor();
+		}
 		
+		String shape = JOptionPane.showInputDialog("What shape do you want to be drawn?");
+		if(shape.equals("square")) {
+			drawSquare();
+		}
+		else if(shape.equals("triangle")) {
+			drawTriangle();
+		}
+		else {
+			drawCircle();
+		}
+		}
+	public static void drawSquare() {
+		for(int i = 0; i < 4; i++) {
+			kate.move(100);
+			kate.turn(90);			
+		}
 	}
-
-	private static void drawSquare() {
-		// TODO Auto-generated method stub
-		
+	public static void drawTriangle() {
+		for(int i = 0; i < 3; i++) {
+			kate.move(100);
+			kate.turn(120);
+		}
 	}
-
+	public static void drawCircle() {
+		for(int i = 0; i < 360; i++) {
+			kate.move(5);
+			kate.turn(1);
+		}
+	}
 }
